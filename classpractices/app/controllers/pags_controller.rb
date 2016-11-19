c9005a025516d2cddb864c73b04f13d866f208b3
FAVIO VELEZ <favio.velez@hotmail.com>
2016-11-18 21:26:19 -0600

    funcionalidad terminada

--- classpractices/app/controllers/pags_controller.rb
+++ classpractices/app/controllers/pags_controller.rb
@@ -10,9 +10,9 @@ class PagsController < ApplicationController
     @hospital = Hospital.new(name: params[:hospital][:name], fed_id: params[:hospital][:fed_id], address: params[:hospital][:address], age: convert_to_age, phone: params[:hospital][:phone], social_security_n: params[:hospital][:social_security_n])
     if params[:hospital][:name].present? && params[:hospital][:address].present? && params[:hospital][:age].present? && params[:hospital][:social_security_n].present?
       @hospital.save
-      render pags_form_path, alert: gen_message
+      render pags_index_path, alert: gen_message
     else
-      render pags_form_path, alert: 'Hizo falta un dato, por favor ingrese Nombre, dirección, edad y Número de IMSS'
+      render pags_index_path, alert: 'Hizo falta un dato, por favor ingrese Nombre, dirección, edad y Número de IMSS'
     end
   end
 
@@ -32,4 +32,5 @@ class PagsController < ApplicationController
     end
     message
   end
+
 end
--- classpractices/app/helpers/pags_helper.rb
+++ classpractices/app/helpers/pags_helper.rb
@@ -1,2 +1,9 @@
 module PagsHelper
+
+  def last_age
+    if params[:hospital].present?
+      return params[:hospital][:age]
+    end
+    ''
+  end
 end
