cab8d30ecdb19fc572791d9839e218709f3fda13
FAVIO VELEZ <favio.velez@hotmail.com>
2016-11-18 19:49:28 -0600

    Métodos acomodados

--- classpractices/app/controllers/pags_controller.rb
+++ classpractices/app/controllers/pags_controller.rb
@@ -8,21 +8,28 @@ class PagsController < ApplicationController
 
   def form_post
     if params[:hospital][:name].present? && params[:hospital][:address].present? && params[:hospital][:age].present? && params[:hospital][:social_security_n].present?
-      a = params[:hospital][:age].split('-').map{ |x| x.to_i }
-      @hospital = Hospital.create(name: params[:hospital][:name], fed_id: params[:hospital][:fed_id], address: params[:hospital][:address], age: ((Date.today - Date.new(a[0], a[1], a[2])) /365).to_i, phone: params[:hospital][:phone], social_security_n: params[:hospital][:social_security_n])
-      count_ages
-      redirect_to pags_form_path, alert: "Los datos han sido guardados con éxito, gracias por registrarse. #{@message}"
+      @hospital = Hospital.create(name: params[:hospital][:name], fed_id: params[:hospital][:fed_id], address: params[:hospital][:address], age: calculate_age, phone: params[:hospital][:phone], social_security_n: params[:hospital][:social_security_n])
+      redirect_to pags_form_path, alert: generate_message
     else
       redirect_to pags_form_path, alert: 'Hizo falta un dato, por favor ingrese Nombre, dirección, edad y Número de IMSS'
     end
   end
 
-  def count_ages
-    b = Hospital.all.group_by{|u| u.age }
-    @message = 'Hay '
-    (b.length).times do |n|
-      @message << "#{b.values[n].count} persona(s) con #{b.keys[n]} años. "
+  def age_date_format
+    params[:hospital][:age].split('-').map{ |x| x.to_i }
+  end
+
+  def calculate_age
+    age_array = age_date_format
+    ((Date.today - Date.new(age_array[0], age_array[1], age_array[2])) /365).to_i
+  end
+
+  def generate_message
+    message = 'Los datos han sido guardados con éxito, gracias por registrarse. Hay '
+    Hospital.all.group_by{|u| u.age }.each do |key, value|
+      message << "#{value.count} persona(s) con #{key} años. "
     end
+    message
   end
 
 end
